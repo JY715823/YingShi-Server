f6eb0fd7e5e3d031e4b0a2b33b6a66b61d1fc9f7
jiangshijie_2023413506 <1085060329@qq.com>
2026-04-29 19:51:58 +0800

    init: create spring boot project

---
+++ src/main/java/com/yingshi/server/YingshiServerApplication.java
@@ -0,0 +1,13 @@
+package com.yingshi.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YingshiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YingshiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yingshi/server/YingshiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yingshi.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YingshiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
